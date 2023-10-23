e9f5b89798b20a00641fc3a81898390cd042da68
Jakub Mazanec <jakub@mazanec.dev>
2023-10-23 21:41:36 +0200

    Init repository

---
+++ packages/4/parcel.config.js
@@ -0,0 +1,14 @@
+export default {
+  targets: {
+    "module": {
+      "source": "./src/main.ts",
+      "outputPath": "./dist/main.js",
+      // etc.
+    },
+    "types": {
+      "source": "./src/main.ts",
+      "outputPath": "./dist/main.d.ts",
+      // etc.
+    },
+  }
+}
---
+++ packages/4/src/foo.ts
@@ -0,0 +1,3 @@
+export type Foo = string;
+
+export const foo: Foo = 'foo!';
